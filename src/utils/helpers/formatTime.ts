5b193b7bba89b2c4ced1d500bb7637df9b05a1eb
Michael Taylor <162068037+mctylr-gh@users.noreply.github.com>
2025-08-15 04:01:17 -0300

    Update npm packages / dependencies. This includes updating these npm modules:
     * @babel
     * @emotion
     * some mui packages 5.14.12 to 5.18.0
     * wasm-web
     * acorn
     * bcryptjs
     * better-react-mathjax
     * clsx
     * date-fns (trivial patch required)
     * monaco-editor
     * monaco-vim
     * react-draggable
     * react-syntax-highlighter
     * tss-react
     * api-documenter
     * api-extractor
     * swc (wasm)
     * react-dom 17.0.21 to 17.0.26
     * typescript-eslint
     * copy-webpack-plugin
     * electron 37.2.3 to 37.2.6
     * eslint 8.52.0 to 8.57.1 (2024-09) (current is 9.33,
        9.x released 2024-04)
     * jest
     * prettier
     * webpack
    
    This does not address:
    
     * eslint 8.57.1 to 9.33, I think converting to the new flat config file
       may be the biggest change needed.
     * react v17 (2021-03) to react v19 (2024-12)
     * mui && material v5.0.6 (2021-10) / 5.18.0 (2025-06)
        to v7 (first released 2024-08)
     * typescript 5.8.3 to 5.9.2 - I believe this is minor,
        see: https://devblogs.microsoft.com/typescript/announcing-typescript-5-7/#typedarrays-are-now-generic-over-arraybufferlike
        and https://devblogs.microsoft.com/typescript/announcing-typescript-5-9/#lib.d.ts-changes
     * rehype / remark update (rehype-mathjax, rehype-raw, remark-gfm)
        Not sure how big a chore this one is.
    
    Other outstanding janitor work includes:
    
     * fix a bunch of text formatting, `npm run format:report` /
        `npm run format`
     * address a large number of errors / warnings from eslint

--- src/utils/helpers/formatTime.ts
+++ src/utils/helpers/formatTime.ts
@@ -2,7 +2,7 @@ import { format } from "date-fns";
 
 export function formatTime(fmt: string): string {
   try {
-    return format(new Date(), fmt);
+    return format(Date.now(), fmt);
   } catch (e: unknown) {
     return "format error";
   }
