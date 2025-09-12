ef637e5166eebbe4bf9fb4ff999d35130c2e97fd
Michael Taylor <162068037+mctylr-gh@users.noreply.github.com>
2025-09-12 05:03:20 -0300

    Update jest-related but not jest itself
     * babel-jest
     * babel-plugin-platform-barrels
     * jest-environment-jsdom
    
    Does **not** include updating jest from v29.7.0 to v30.1.0
    as that seems to tickle some config / build envionment bugs.

--- test/jest/config/babelTransform.js
+++ test/jest/config/babelTransform.js
@@ -1,16 +1,22 @@
 const babelJest = require("babel-jest").default;
 
+function normalizeExtensions(moduleFileExtensions, defaultPlatform) {
+  if (!defaultPlatform) return moduleFileExtensions;
+  const normalizedExtensions = moduleFileExtensions.map(ext => `${defaultPlatform}.${ext}`);
+  return [...moduleFileExtensions, ...normalizedExtensions];
+}
+
 module.exports = {
   process(sourceText, sourcePath, options) {
     const alias = options?.config?.moduleNameMapper;
-    const extensions = options.config.moduleFileExtensions;
+    const extensions = normalizeExtensions(options.config.moduleFileExtensions, options?.config?.haste?.defaultPlatform);
+    const modulesDirs = [...options.config.moduleDirectories, ...(options.config.modulePaths || [])];
     const babelTransformer = babelJest.createTransformer({
-      presets: [["@babel/preset-react"], ["@babel/preset-env"], ["@babel/preset-typescript"]],
-      plugins: [["transform-barrels", { executorName: "jest", alias: alias, extensions: extensions }]],
+      presets: [["@babel/preset-typescript"], ["@babel/preset-react"], ["@babel/preset-env"]],
+      plugins: [["transform-barrels", { executorName: "jest", alias: alias, extensions: extensions, modulesDirs: modulesDirs }]],
       babelrc: false,
       configFile: false,
     });
-
     return babelTransformer.process(sourceText, sourcePath, options);
   },
 };
