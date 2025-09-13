d15e745803100f544a96a8c64256de81702fc83a
Michael Taylor <162068037+mctylr-gh@users.noreply.github.com>
2025-09-13 03:54:30 -0300

    Update package-electron to handle macOS (aka darwin) on arm64.
    As of electron 38, which used Chromium 140, macOS 11 (BigSur)
    is no longer supported. Chromium 140 no longer supports macOS 11,
    and so Electron 38 and newer won't as well.
    
    While you can use older versions of Electron (36, 37) & thus older
    versions of Chromium, as I understand it, macOS 12 (Monterey) support
    will require Electron 38 or newer. Meaning we can't support both macOS
    versions with a single electron version if I understand correctly.
    
    I won't have access to *any* macOS systems, so I can't verify that
    the resultant builds work, but at least the @electron/packager
    runs and outputs something.
    
    It still doesn't deal with app signing for any platform.
    
    The script itself is cleaned up as well, with things like
    variables to make it easier to read.

--- tools/package-electron.sh
+++ tools/package-electron.sh
@@ -1,33 +1,56 @@
 #!/bin/bash
 
+YEAR=2025
+COPYRIGHT="Copyright ©${YEAR} Bitburner"
+PACKAGER="npx @electron/packager"
+
 # Clear out any files remaining from old builds and recreate folder
 rm -rf .package
-mkdir .package
+mkdir .package || exit 2
 rm -rf .build
-mkdir .build
+mkdir .build || exit 2
 
 # Install electron sub-dependencies
-cd electron
-npm install
+cd electron || exit 2
+npm install || exit 2
 cd ..
 
 # .app should have the fully built game already after npm run build
-cp -r .app/* .package
+if [ -d .app ]; then
+  cp -r .app/* .package || exit 2
+else
+  echo "Need to build app first. npm run build" && exit 1
+fi
 cp -r electron/* .package
 
 packageWin() {
-  electron-packager .package bitburner --platform win32 --arch x64,arm64 --out .build --overwrite --icon .package/icon.ico --app-copyright "Copyright (C) 2025 Bitburner"
+  eval "$(package win32 x64,arm64 .package/icon.ico)"
 }
 
 packageLinux() {
-  electron-packager .package bitburner --platform linux --arch x64,arm64 --out .build --overwrite --app-copyright "Copyright (C) 2025 Bitburner"
+  eval "$(package linux x64,arm64)"
 }
 
 packageMac() {
-  electron-packager .package bitburner --platform darwin --arch universal --osx-universal.x64ArchFiles="Contents/Resources/app/node_modules/@catloversg/steamworks.js/dist/osx/*" --out .build --overwrite --icon .package/icon.icns --app-copyright "Copyright (C) 2025 Bitburner"
+  UNI_ARGS='--osx-universal.x64ArchFiles="Contents/Resources/app/node_modules/@catloversg/steamworks.js/dist/osx/*" '
+  eval "$(package darwin arm64 .package/icon) ${UNI_ARGS}"
 }
 
-BUILD_PLATFORM="${1:-"all"}"
+package() {
+  PLATFORM=$1
+  ARCH=$2
+  ICON=$3
+
+  if [ -n "$ICON" ]; then     # Only include icon flag if icon file specified
+    ICONARG="--icon ${ICON}"
+  else
+    ICONARG=""
+  fi
+
+  echo "${PACKAGER} .package bitburner --platform ${PLATFORM} --arch ${ARCH} --overwrite --out .build ${ICONARG} --app-copyright ${COPYRIGHT}"
+}
+
+BUILD_PLATFORM=${1:-"all"}
 # And finally build the app.
 case $BUILD_PLATFORM in
   "win")
@@ -41,3 +64,6 @@ case $BUILD_PLATFORM in
     packageLinux;
     packageMac;;
 esac
+
+# Cleanup temporary files
+## rm -rf .package
